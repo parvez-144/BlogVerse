b6b72e28cfe1faeedeb3d2407d011871dd45ff3d
parvez-144 <parvezrazasiddiqui444@gmail.com>
2024-03-06 10:00:46 +0530

    bug resolved

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import AddPost from "./pages/AddPost";
 import Signup from './pages/Signup'
 import EditPost from "./pages/EditPost";
 
-import Post from './pages/post'
+import Post from './pages/Post'
 
 import AllPosts from "./pages/AllPosts";
 
